81b0e68d41e1e1bbdeb90269e698851380527e50
Saugat Sthapit <mac@n3-28-82.dhcp.drexel.edu>
2017-02-25 14:15:03 -0500

    Updated1

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 var express = require('express');
-var app = express()
+var path = require('path');
+var app = express();
 
-app.use(express.static('.'))
+
+
+app.use(express.static(path.join(__dirname, '/')));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html', null, function(error, data) { //getting the file
+        if (error) {
+            res.writeHead(404);
+            res.write("File not found");
+        }
+    });
+});
+
+app.get('/itemdetails', function(req, res){
+
+})
+
+app.get('/nettrade_login', function(req, res){
+
+})
+
+app.get('/add_new_product', function(req, res){
+
+})
+
+app.get('/userprofile', function(req, res){
+
+})
+
+
+
 
 app.listen(8080, function() {
   console.log("Server is listening on localhost:8080.")
