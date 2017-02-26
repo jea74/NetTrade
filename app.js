02013fc54def6c74c2ec659ca618589c0b05850c
jyoo9326 <Jiho.Yoo@drexel.edu>
2017-02-25 21:50:17 -0500

    getting product info

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+var http=require('http');
+var $=require('jquery');
 var express = require('express');
 var path = require('path');
 var app = express();
-
-
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'Root1!',
+	database: 'netTrade',
+});
+con.connect(function(err) {
+	if(err) {
+		console.log("Error connecting to database");
+	}
+	else {
+		console.log("Database successfully connected");
+	}
+});
 
 app.use(express.static(path.join(__dirname, '/')));
 app.get('/', function(req, res) {
@@ -23,7 +38,21 @@ app.get('/nettrade_login', function(req, res){
 })
 
 app.get('/add_new_product', function(req, res){
-
+	var category = req.query.category;
+	var product_name= req.query.name;
+	var desc=req.query.product_desc;
+	var product_price=req.query.price;
+	var image=req.query.image;
+var product = { name: product_name, photoURL: image, price: product_price, description: desc, providerID: "FACEBOOK.COM"};
+	console.log(product);
+var query = con.query('insert into productt set ?', product, function(err,rows,fields) {
+if (err)
+console.log('Error during query processing');
+else
+console.log('Here is the result : ', rows);
+});
+res.send('ALL');
+ 
 })
 
 app.get('/userprofile', function(req, res){
